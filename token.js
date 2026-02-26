0d015bb926077eed6a37cfc3f8062f8c9389057f
asytzy-dev <muhammadasradpratama@gmail.com>
2026-02-26 22:42:28 +0700

    Update token.js

--- token.js
+++ token.js
@@ -1,5 +1,5 @@
 [ 
-"120363425185712509@newsletter",
-"120363403554600822@newsletter",
-"120363402450800056@newsletter"
+"120363425185712509@newsletter", // zebuild
+"120363403554600822@newsletter", // zebuild 2 
+"120363402450800056@newsletter" // vella 
 ]
